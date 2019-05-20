2bf07db7ba957b23a5dceabbee423bac33491cf3
Kura <linra.kura@gmail.com>
2019-05-19 18:00:19 -0700

    foramt

--- src/main.c
+++ src/main.c
@@ -16,59 +16,59 @@ int                 g_recvfd;
 
 void 	clean_up(void)
 {
-    if (g_recvfd >=0)
-	    close(g_recvfd);
-    if (g_sendfd >=0)
-	    close(g_sendfd);
-    if (g_addrinfo)
-	    freeaddrinfo(g_addrinfo);
+	if (g_recvfd >=0)
+		close(g_recvfd);
+	if (g_sendfd >=0)
+		close(g_sendfd);
+	if (g_addrinfo)
+		freeaddrinfo(g_addrinfo);
 }
 
 void 	creat_sock(void)
 {
 	uid_t				uid;
 	struct sockaddr_in	serbind; 
-    struct timeval      timeout;
+	struct timeval      timeout;
 
-    bzero_(&serbind, sizeof(serbind));
+	bzero_(&serbind, sizeof(serbind));
 	if ((uid = getuid()) != 0)
 		FATAL("Need run premission to create raw socket.");
 	if ((g_recvfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1
-        || (g_sendfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-    {
-        clean_up();
-        ERR_QUIT("socket");
-    }
+		|| (g_sendfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+	{
+		clean_up();
+		ERR_QUIT("socket");
+	}
 
-    timeout.tv_sec  = g_waittime;
-    timeout.tv_usec = 0;
-    if (setsockopt(g_recvfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
-                sizeof(timeout)) < 0)
-    {
-        clean_up();
-        ERR_QUIT("setsockopt");
-    }
+	timeout.tv_sec  = g_waittime;
+	timeout.tv_usec = 0;
+	if (setsockopt(g_recvfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
+				sizeof(timeout)) < 0)
+	{
+		clean_up();
+		ERR_QUIT("setsockopt");
+	}
 
 	serbind.sin_family  = AF_INET;
 	g_sport             = (getpid() & 0xffff) | 0x8000;
 	serbind.sin_port    = htons(g_sport);
 	if (bind(g_sendfd, (struct sockaddr *)(&serbind), sizeof(serbind)) == -1)
 	{
-        clean_up();
-        INFO("Bind failed.");
-        ERR_QUIT("bind");
-    }
+		clean_up();
+		INFO("Bind failed.");
+		ERR_QUIT("bind");
+	}
 }
 
 void 	init(void)
 {
-    g_dport         = DEFAULT_PORT_NUM;
+	g_dport         = DEFAULT_PORT_NUM;
 	g_inital_ttl    = INITIAL_TTL;
 	g_max_ttl       = MAX_TTL;
 	g_probes        = DEFAULT_PROBE;
 	g_waittime      = DEFAULT_WAIT;
-    g_recvfd        = g_sendfd = -1;
-    g_loss          = false;
+	g_recvfd        = g_sendfd = -1;
+	g_loss          = false;
 }
 
 int		wait_and_recv(int seq, struct timeval *tv)
@@ -81,14 +81,14 @@ int		wait_and_recv(int seq, struct timeval *tv)
 	struct icmp 	*icmp_hdr;
 	struct udphdr 	*udp_hdr;
 
-    ret = 0;
+	ret = 0;
 	while (true)
 	{
-        b_read = recvfrom(g_recvfd, recvbuf, sizeof(recvbuf), 0, &g_serrecv, (unsigned []){sizeof(g_serrecv)});
+		b_read = recvfrom(g_recvfd, recvbuf, sizeof(recvbuf), 0, &g_serrecv, (unsigned []){sizeof(g_serrecv)});
 		if (b_read < 0)
 		{
 			if (errno == EAGAIN)
-                return (SOCK_TIMEOUT);
+				return (SOCK_TIMEOUT);
 			else
 				perror_("recvfrom");
 		}
@@ -109,18 +109,18 @@ int		wait_and_recv(int seq, struct timeval *tv)
 				continue ;
 
 			udp_hdr = (struct udphdr *)(recvbuf + ip_out_len + ICMP_HDR_LEN + ip_in_len);
-      
+	  
 			if (ip_hdr_in->ip_p == IPPROTO_UDP
 				&& udp_hdr->uh_sport == htons(g_sport)
 				&& udp_hdr->uh_dport == htons(g_dport + seq))
 			{
-                ret = (icmp_hdr->icmp_code);
-                break ;
-            }
+				ret = (icmp_hdr->icmp_code);
+				break ;
+			}
 		}
 	}
-    gettimeofday(tv, NULL);
-    return (ret);
+	gettimeofday(tv, NULL);
+	return (ret);
 }
 
 void 	traceroute(void)
@@ -131,30 +131,30 @@ void 	traceroute(void)
 	double					rtt;
 	uint16_t				seq;
 	int						code;
-    int                     b_sent;
-    uint8_t                 pack_lost;
+	int                     b_sent;
+	uint8_t                 pack_lost;
 	char					sendbuf[777];
 	char					hostname[NI_MAXHOST];
-    bool                    arrived;
-    
-    printf("traceroute to %s (%s), %d hops max, %ld byte packets\n",
-                            g_hostname, 
-                            inet_ntoa(((struct sockaddr_in *)g_addrinfo->ai_addr)->sin_addr),
-                            g_max_ttl,
-                            sizeof(struct s_content));
-    
-    seq = 0;
-    arrived = false;
-    pack_lost = 0;
-    (void)b_sent;
-    for (u_int8_t ttl = g_inital_ttl; ttl <= g_max_ttl; ttl++)
+	bool                    arrived;
+	
+	printf("traceroute to %s (%s), %d hops max, %ld byte packets\n",
+							g_hostname, 
+							inet_ntoa(((struct sockaddr_in *)g_addrinfo->ai_addr)->sin_addr),
+							g_max_ttl,
+							sizeof(struct s_content));
+	
+	seq = 0;
+	arrived = false;
+	pack_lost = 0;
+	(void)b_sent;
+	for (u_int8_t ttl = g_inital_ttl; ttl <= g_max_ttl; ttl++)
 	{
 		ERR_CHECK(setsockopt(g_sendfd, IPPROTO_IP, IP_TTL, (int[]){(int)ttl}, sizeof(int)) == -1,\
 			"setsockopt");
 		bzero_(&serlast, sizeof(serlast));
 		printf("%2d ", ttl);
 		fflush(stdout);
-        pack_lost ^= pack_lost;
+		pack_lost ^= pack_lost;
 		for (uint8_t probe = 0; probe < g_probes; probe++)
 		{
 			data        = (struct s_content *)(sendbuf);
@@ -165,12 +165,12 @@ void 	traceroute(void)
 			((struct sockaddr_in *)g_addrinfo->ai_addr)->sin_port = htons(g_dport + seq);
 			b_sent = sendto(g_sendfd, sendbuf, sizeof(struct s_content), 0, g_addrinfo->ai_addr, g_addrinfo->ai_addrlen);
 
-            code = wait_and_recv(seq, &recvtv);
+			code = wait_and_recv(seq, &recvtv);
 			if (code == SOCK_TIMEOUT)
 			{
-                pack_lost++;
-                printf (" *");
-            }
+				pack_lost++;
+				printf (" *");
+			}
 			else 
 			{
 				if (serlast.sin_addr.s_addr != ((struct sockaddr_in *)&g_serrecv)->sin_addr.s_addr)
@@ -185,24 +185,24 @@ void 	traceroute(void)
 				rtt = recvtv.tv_sec * 1000.0 + recvtv.tv_usec / 1000.0;
 				printf(" %.3f ms", rtt);
 			}
-            if (code == ICMP_UNREACH_PORT)
-                arrived = true;
-            else if (code != ICMP_TIMXCEED_INTRANS && code != SOCK_TIMEOUT)
+			if (code == ICMP_UNREACH_PORT)
+				arrived = true;
+			else if (code != ICMP_TIMXCEED_INTRANS && code != SOCK_TIMEOUT)
 				printf (" (ICMP %d)", code);
 
-            fflush(stdout);
+			fflush(stdout);
 		}
-        if (g_loss)
-            printf(" (%.1f%% loss)", ((float)pack_lost / g_max_ttl * 100.0) * 10);
-        printf("\n");
-        if (arrived)
-		    break ;
+		if (g_loss)
+			printf(" (%.1f%% loss)", ((float)pack_lost / g_max_ttl * 100.0) * 10);
+		printf("\n");
+		if (arrived)
+			break ;
 	}
 }
 
 int		main(int ac, char **av)
 {
-    init();
+	init();
 	readopt(ac, av);
 	creat_sock();
 	traceroute();
